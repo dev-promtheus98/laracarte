5f3e3541074b91e0fcf8155f029603f06fcc361e
dev-promtheus98 <ozh98@protonmail.com>
2020-05-12 01:06:51 +0000

    Configure App name

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="text-center">
     <p>
-        &copy; {{ date('Y') }} &middot; Laracarte by @zana_Ouattara - A Laramap clone App
+        &copy; {{ date('Y') }} &middot; {{ config('app.name') }} by @zana_Ouattara - A Laramap clone App
     </p>
     <p>
         <b> this app has been built for learning purposes. </b>
